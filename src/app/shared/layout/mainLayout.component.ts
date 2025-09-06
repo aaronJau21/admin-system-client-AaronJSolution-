b018473812aa9d69937f4c53090a25c4eb170280
aaronJau21 <aaronjau21@gmail.com>
2025-09-06 17:09:07 -0500

    Refactor layout styles and structure for main layout component

--- src/app/shared/layout/mainLayout.component.ts
+++ src/app/shared/layout/mainLayout.component.ts
@@ -4,7 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatListModule } from '@angular/material/list';
+import { MatListItem, MatListModule } from '@angular/material/list';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
